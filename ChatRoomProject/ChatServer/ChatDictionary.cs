2b3b35bb0d15dfb2f7e988ab02c467759dbb7d5c
nlthorne <nlthorne@wi.rr.com>
2016-10-04 11:26:45 -0500

    added classes and server methods

---
+++ ChatRoomProject/ChatServer/ChatDictionary.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Chat = System.Net;
+
+namespace ChatServer
+{
+    class ChatDictionary
+    {
+        public Dictionary<string, Chat.Sockets.TcpClient> ClientsByName = new Dictionary<string, Chat.Sockets.TcpClient>();
+        public Dictionary<Chat.Sockets.TcpClient, string> ClientsByNumber = new Dictionary<Chat.Sockets.TcpClient, string>();
+    }
+}
---
+++ ChatRoomProject/ChatServer/Server.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Net;
+using System.Threading;
+using Chat = System.Net;
+using System.Collections;
+
+namespace ChatServer
+{
+    class Server
+    {
+        System.Net.Sockets.TcpListener chatServer;
+        ChatDictionary userDictionary = new ChatDictionary();
+        Listener listener = new Listener();
+        Writer writer = new Writer();
+        Reader reader = new Reader();
+
+        public void ConnectClients()
+        {
+            while (true)
+            {
+                listener.serverListener.Start();
+                if (listener.serverListener.Pending())
+                {
+                    Chat.Sockets.TcpClient connectedClient = listener.serverListener.AcceptTcpClient();
+                    ChatRoom chatRoom = new ChatRoom(connectedClient, userDictionary, this);
+                }
+            }
+        }
+    }
+}
